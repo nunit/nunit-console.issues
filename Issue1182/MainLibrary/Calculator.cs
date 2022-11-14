b05dbc66a340563e950347b302aeeddbf511b42c
Charlie Poole <charliepoole@gmail.com>
2022-11-14 11:23:20 -0800

    Issue 1182

---
+++ Issue1182/MainLibrary/Calculator.cs
@@ -0,0 +1,10 @@
+﻿namespace MainLibrary
+{
+    public class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ Issue1182/TestLibrary/TestCalculator.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace TestLibrary
+{
+    [TestFixture]
+    public class TestCalculator
+    {
+        [Test]
+        public void Add()
+        {
+            Assert.Pass();
+        }
+    }
+}
