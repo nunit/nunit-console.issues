bc145394d0b04b5f6e1f9cad04ad2009c7b72553
Charlie Poole <charliepoole@gmail.com>
2022-11-14 11:21:05 -0800

    Issue 291

---
+++ Issue291/TestWithAdditionalPackageRef/Class1.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using Newtonsoft.Json;
+using System;
+
+namespace TestWithAdditionalPackageRef
+{
+    public class Class1
+    {
+        [Test]
+        public void SimpleTest()
+        {
+            Assert.Pass("SimpleTest passed!");
+        }
+
+        [Test]
+        public void JsonTest()
+        {
+            var stringWriter = new System.IO.StringWriter();
+            var jsonWriter = new JsonTextWriter(stringWriter);
+            jsonWriter.WriteStartObject();
+            jsonWriter.WriteComment("Just a comment");
+            jsonWriter.WriteEndObject();
+
+            Assert.That(stringWriter.ToString(), Is.EqualTo("{/*Just a comment*/}"));
+        }
+    }
+}
---
+++ Issue291/TestWithNunitRefOnly/Class1.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace AutoFacTest
+{
+    public class Class1
+    {
+        [Test]
+        public void SimpleTest()
+        {
+            Assert.Pass("SimpleTest passed!");
+        }
+    }
+}
